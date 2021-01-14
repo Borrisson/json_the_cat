63c6d13945827c013af93fda99f90c581b22dba0
Borrisson <lucborris@gmail.com>
2021-01-14 18:36:39 +0000

    added test conditions for files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,9 @@
 const request = require('request');
 const fetchBreedDescription = (catBreed, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-    const data = JSON.parse(body);
-    if (!error && data.length) {
-      callback(null, data[0].description);
+    const data = JSON.parse(body)[0];
+    if (!error && data) {
+      callback(null, data.description);
     } else if (error) {
       callback('There was an error with your request:  ' + error);
     } else {
